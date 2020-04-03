048c20873144c608f9c1674a9dd3e60bc04da86d
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-04-03 15:13:14 -0700

    added graph iterative solution

--- graphs.js
+++ graphs.js
@@ -25,21 +25,85 @@ class Graph {
     delete this.adjacencyList[vert]
   }
 
+
+  dfTrav(vert) {
+    if (!vert) return null
+    const visited = {}
+    let order = []
+    order = order.concat(this.dfTravRecursive(vert, visited))
+    return order
+  }
+
+  dfTravRecursive(vert, visited) {
+    visited[vert] = true
+    let order = []
+    order.push(vert)
+    for (let adj of this.adjacencyList[vert]) {
+      if (visited[adj] === undefined) {
+        order = order.concat(this.dfTravRecursive(adj, visited))
+      }
+    }
+    return order
+  }
+
+  dfTravIterate(vert) {
+    const visited = {}
+    const order = []
+    const stack = []
+    stack.push(vert)
+    while (stack.length > 0) {
+      let popped = stack[stack.length-1]
+      if (!visited[popped]) {
+        visited[popped] = true
+        order.push(popped)
+        for (let adj of this.adjacencyList[popped]) {
+          stack.push(adj)
+        }
+      } else {
+        stack.pop()
+      }
+    }
+    return order
+  }
+
+
 }
 
 
-let newGraph = new Graph()
+// const newGraph = new Graph()
 
-newGraph.addVertex("Tokyo")
-newGraph.addVertex("Dallas")
-newGraph.addVertex("Aspen")
+// newGraph.addVertex("Tokyo")
+// newGraph.addVertex("Dallas")
+// newGraph.addVertex("Aspen")
 
-newGraph.addEdge('Tokyo', 'Dallas')
-newGraph.addEdge('Dallas', 'Aspen')
-console.log(newGraph.adjacencyList)
+// newGraph.addEdge('Tokyo', 'Dallas')
+// newGraph.addEdge('Dallas', 'Aspen')
+// console.log(newGraph.adjacencyList)
 
-newGraph.removeEdge('Tokyo', 'Dallas')
-console.log(newGraph.adjacencyList)
+// newGraph.removeEdge('Tokyo', 'Dallas')
+// console.log(newGraph.adjacencyList)
 
-newGraph.removeVertex('Aspen')
-console.log(newGraph.adjacencyList)
+// newGraph.removeVertex('Aspen')
+// console.log(newGraph.adjacencyList)
+
+const dfsGraph = new Graph()
+dfsGraph.addVertex("A")
+dfsGraph.addVertex("B")
+dfsGraph.addVertex("C")
+dfsGraph.addVertex("D")
+dfsGraph.addVertex("E")
+dfsGraph.addVertex("F")
+dfsGraph.addVertex("G")
+
+dfsGraph.addEdge("A", "B")
+dfsGraph.addEdge("A", "C")
+dfsGraph.addEdge("B", "D")
+dfsGraph.addEdge("C", "E")
+dfsGraph.addEdge("D", "E")
+dfsGraph.addEdge("D", "F")
+dfsGraph.addEdge("E", "F")
+dfsGraph.addEdge("F", "G")
+console.log(dfsGraph.adjacencyList)
+console.log(dfsGraph.dfTrav("A"))
+// console.log(dfsGraph.dfTrav("B"))
+console.log(dfsGraph.dfTravIterate("A"))
